727432a50aa2c9f1b98bbe95d3e0a218e229821c
muiruriiii <muiruricynthiaaa@gmail.com>
2022-10-07 20:25:24 +0300

    Cart Functionality Done

--- shop/urls.py
+++ shop/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.store, name="store"),
     path('cart/', views.cart, name="cart"),
+    path('update_item/', views.updateItem, name="checkout"),
     path('checkout/', views.checkout, name="checkout"),
 ]
