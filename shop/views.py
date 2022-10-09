9ab5de550f884c97314f5df054037be9a47b4725
muiruriiii <muiruricynthiaaa@gmail.com>
2022-10-09 16:06:12 +0300

    Code Clean Up on views.py

--- shop/views.py
+++ shop/views.py
@@ -4,7 +4,7 @@ from django.http import JsonResponse
 import json
 import datetime
 
-from . utils import cookieCart, cartData
+from . utils import cookieCart, cartData, guestOrder
 
 
 # Create your views here.
@@ -73,51 +73,23 @@ def processOrder(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        total = float(data['form']['total'])
-        order.transaction_id = transaction_id
-
-        if total == order.get_cart_total:
-            order.complete = True
-        order.save()
-
-        if order.shipping == True:
-            ShippingAddress.objects.create(
-                customer=customer,
-                order=order,
-                address=data['shipping']['address'],
-                city=data['shipping']['city'],
-                state=data['shipping']['state'],
-                zipcode=data['shipping']['zipcode'],
-            )
 
     else:
-        print('User is not logged in...')
+        customer, order = guestOrder(request, data)
 
-        print('COOKIES:', request.COOKIES)
-        name = data['form']['name']
-        email = data['form']['email']
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
 
-        cookieData = cookieCart(request)
-        items = cookieData['items']
-
-        customer, created = Customer.objects.get_or_create(
-            email = email,
-        )
-        customer.name = name
-        customer.save()
-
-        order = Order.objects.create(
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
+    if order.shipping == True:
+        ShippingAddress.objects.create(
             customer=customer,
-            complete=False,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
         )
-
-        for item in items:
-            product = Product.objects.get(id=item['product']['id'])
-
-            orderItem = OrderItem.objects.create(
-                product=product,
-                order=order,
-                quantity=item['quantity']
-            )
-
     return JsonResponse('Payment submitted..', safe=False)
